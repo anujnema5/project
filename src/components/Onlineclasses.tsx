9e9c655a98b78d9b0fc502961adbf3cfc2f18b96
Anuj nema <anujnema63@gmail.com>
2023-09-24 13:45:16 +0530

    minor responsiveness fixed

--- src/components/Onlineclasses.tsx
+++ src/components/Onlineclasses.tsx
@@ -12,9 +12,9 @@ function Onlineclasses({ info }: { info: string }) {
     <div className='relative w-full h-2/5 lg:py-16 py-10 bg-class-banner bg-cover bg-no-repeat bg-fixed flex justify-center items-center'>
       <div className="overlay absolute top-0 left-0 w-full h-full bg-black/80 bg-cover flex items-center justify-center"></div>
 
-        <div className="flex lg:px-0 px-5 lg:w-2/3 w-full gap-6 items-center lg:flex-row flex-col">
+        <div className="flex lg:px-0 px-5 2xl:w-8/12 xl:w-10/12  w-11/12 gap-6 items-center lg:flex-row flex-col">
           <div className="h-auto w-full z-30 prose-h2:text-white prose-p:text-base prose-p:text-gray-100 lg:w-2/4 lg:text-left text-center">
-            <div className="prose w-full">
+            <div className="prose prose-h2:text-3xl w-full mx-auto lg:mx-0">
             <Content />
             </div>
             <button className='bg-gradient-to-r mt-4 from-pink-700 to-blue-800 rounded-3xl py-1.5 px-4 text-gray-200'>Read More</button>
@@ -23,8 +23,8 @@ function Onlineclasses({ info }: { info: string }) {
               <h3 className='text-lg font-semibold'>Download The App</h3>
 
               <div className="flex gap-3 lg:justify-start justify-center items-center w-auto pt-2">
-                <Image src={'https://www.nifafinearts.com/images/gplay.png'} alt='play-store' width={1000} height={1000} className='lg:w-4/12 lg:h-14 w-5/12 sm:w-4/12'/>
-                <Image src={'https://www.nifafinearts.com/images/appstore.png'} alt='play-store' width={1000} height={1000} className='lg:w-4/12 lg:h-14 w-5/12 sm:w-4/12'/>
+                <Image src={'https://www.nifafinearts.com/images/gplay.png'} alt='play-store' width={160} height={160} className=''/>
+                <Image src={'https://www.nifafinearts.com/images/appstore.png'} alt='play-store' width={160} height={160} className=''/>
               </div>
             </div>
           </div>
