803dd8da6e4136ced6d472f91e2ade52c6080db4
Anuj nema <anujnema63@gmail.com>
2023-09-24 14:35:05 +0530

    minor fixes

--- src/components/Courses.tsx
+++ src/components/Courses.tsx
@@ -4,7 +4,7 @@ import Coursescard from './common/Coursescard'
 
 function Courses() {
     return (
-        <div className='h-auto w-full flex flex-col lg:py-14 py-5  bg-courses-banner bg-cover bg-no-repeat'>
+        <div className='h-auto w-full flex flex-col lg:py-14 pt-10  bg-courses-banner bg-cover bg-no-repeat'>
             <h1 className='text-gray-800 mx-auto font-bold text-2xl'>Our Courses</h1>
             <div className="2xl:w-10/12 xl:w-11/12 lg:px-10 px-0  mx-auto flex flex-wrap gap-7 py-10 justify-center ">
                 {courses.map((course) => (
